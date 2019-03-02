51f174c330290c45b03f69d2ba3b6703e9ea2af9
Ruoxuan Han <r9han@uwaterloo.ca>
2019-03-02 17:36:08 -0500

    added converter and note class

---
+++ src/Note.java
@@ -0,0 +1,26 @@
+
+
+public class Note {
+	private final int STANDARD_A4 = 440;
+	int keyNumber;
+	int octave;
+	String note;
+	
+	public void print() {
+		System.out.println("keyNumber: " + keyNumber);
+		System.out.println("octave: " + octave);
+		System.out.println("note: " + note);
+		System.out.println();
+	}
+
+	public Note(int keyNumber, int octave, String note) {
+		super();
+		this.keyNumber = keyNumber;
+		this.octave = octave;
+		this.note = note;
+	}
+	
+	
+	
+
+}
